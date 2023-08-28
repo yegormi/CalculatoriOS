4e7b2970d94f72313c928c575e183be1f7f0d905
yegormi <egormiropoltsev79@gmail.com>
2023-08-28 15:17:35 +0300

    Initial Commit

---
+++ Calculator/CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalculatorApp.swift
+//  Calculator
+//
+//  Created by Yegor Myropoltsev on 28.08.2023.
+//
+
+import SwiftUI
+
+@main
+struct CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
