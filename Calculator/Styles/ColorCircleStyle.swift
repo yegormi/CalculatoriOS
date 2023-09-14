a5df583e2ec424077caa1b583cfe397d36350a0c
yegormi <egormiropoltsev79@gmail.com>
2023-09-14 18:27:34 +0300

    color circle preview

--- Calculator/Styles/ColorCircleStyle.swift
+++ Calculator/Styles/ColorCircleStyle.swift
@@ -11,6 +11,7 @@ import SwiftUI
 struct ColorCircle: View {
     let colorSelected: ColorPicker
     let isSelected: Bool
+    let labelText: String
     
     let circleSize: CGFloat = 20
     var innerSize: CGFloat {
@@ -25,19 +26,45 @@ struct ColorCircle: View {
             Circle()
                 .fill(colorSelected.color)
                 .frame(width: buttonSize, height: buttonSize)
-            
-            if isSelected {
-                Circle()
-                    .fill(Color.white)
-                    .frame(width: innerSize, height: innerSize)
-            }
+                .overlay(
+                    isSelected ?
+                        Circle()
+                            .fill(Color.white)
+                            .frame(width: innerSize, height: innerSize) : nil
+                    
+                )
+            Text(labelText)
+                .font(.system(size: 15, weight: .regular))
+                .lineLimit(1)
+                .foregroundColor(Color.gray)
+//                .frame(width: buttonSize)
+                .minimumScaleFactor(0.5)
+                .offset(y: circleSize * 2)
         }
     }
     
-    private func getButtonSize() -> CGFloat {
+    private func getButtonSize() -> Double {
         let screenWidth = UIScreen.main.bounds.width
-        let buttonCount: CGFloat = 7.0
+        let buttonCount = 7.0
         let spacingCount = buttonCount + 1
         return (screenWidth - (spacingCount * 12)) / buttonCount
     }
 }
+
+struct ColorCircle_Previews: PreviewProvider {
+    static var previews: some View {
+        LazyHStack(spacing: 20) {
+            Group {
+                ColorCircle(colorSelected: .red, isSelected: false, labelText: "Red")
+                    .previewLayout(.sizeThatFits)
+                
+                ColorCircle(colorSelected: .orange, isSelected: true, labelText: "Orange")
+                    .previewLayout(.sizeThatFits)
+            }
+        }
+        .previewLayout(PreviewLayout.sizeThatFits)
+        .padding()
+        .previewDisplayName("Default preview")
+    }
+}
+
